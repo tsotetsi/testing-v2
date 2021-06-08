5d114492763c72b7bb9f14bd135ec94dbf12c4d5
Tsotetsi <me@tsotetsithapeloo.co.za>
2021-06-08 10:10:50 +0200

    Adding mini-calculator file.

---
+++ main.py
@@ -0,0 +1,12 @@
+class MyMiniCalculator():
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def devide(self, a, b):
+        return a / b
